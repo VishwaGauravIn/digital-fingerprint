9ba895cb9bd7c6650ca4f6b18f70fb0a8388cf4c
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-07-23 11:03:25 +0530

    layout structure

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function Layout() {
+export default function Layout({ row, col, imgSrc }) {
   return (
-    <div>Layout</div>
-  )
+    <div
+      className={`flex ${
+        col == "reverse" ? "flex-col-reverse" : "flex-col"
+      } md:${row == "reverse" ? "flex-row-reverse" : "flex-row"} items-center`}
+    >
+      <div className="w-full md:w-6/12">{children}</div>
+      <div className="w-full md:w-6/12">
+        <img
+          src={imgSrc}
+          alt="image"
+          className="w-full select-none pointer-events-none"
+          draggable={false}
+        />
+      </div>
+    </div>
+  );
 }
