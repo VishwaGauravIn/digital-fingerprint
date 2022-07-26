9c78957820bb0a245446e9ff2e49fab38f03d5d9
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-07-26 16:20:30 +0530

    tried to fix to http req

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import axios from "axios";
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  axios.get("http://www.geoplugin.net/json.gp/").then((r) => {
+    res.status(200).json(r.data);
+  });
 }
